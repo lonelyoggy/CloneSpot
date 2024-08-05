b5e6b1e9f6905f83c23791762f6b41d15f0d752d
lonelyoggy <phuclinhvip1000@gmail.com>
2024-08-05 10:49:36 +0700

    update main style and layout

--- src/Components/main/main.js
+++ src/Components/main/main.js
@@ -1,3 +1,4 @@
+// Main.js
 import React, { useEffect, useState } from 'react';
 import './main.css';
 import axios from 'axios';
@@ -8,54 +9,38 @@ const Main = ({ user, token }) => {
   useEffect(() => {
     const fetchRecommendations = async () => {
       try {
-        // Fetch the user's top tracks
         const topTracksResponse = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
+          headers: { Authorization: `Bearer ${token}` }
         });
 
-        const topTracks = topTracksResponse.data.items;
-        
+        const trackIds = topTracksResponse.data.items.slice(0, 5).map(track => track.id).join(',');
 
-        // Get track IDs
-        const trackIds = topTracks.slice(0, 5).map(track => track.id).join(',');
-        console.log('Track IDs for recommendations:', trackIds);
-
-        // Fetch recommendations based on top tracks
         const recommendationsResponse = await axios.get(`https://api.spotify.com/v1/recommendations?seed_tracks=${trackIds}`, {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
+          headers: { Authorization: `Bearer ${token}` }
         });
 
-        console.log('Fetched recommendations:', recommendationsResponse.data.tracks);
         setRecommendations(recommendationsResponse.data.tracks);
       } catch (error) {
-        console.log(error);
+        console.error('Error fetching recommendations:', error);
       }
     };
 
-    if (token) {
-      fetchRecommendations();
-    }
+    if (token) fetchRecommendations();
   }, [token]);
 
   return (
-    <div className="content">
-      <h2>Good Afternoon{user ? `, ${user.display_name}` : ''}</h2>
-      <div className="card-container">
-        {recommendations.length > 0 ? (
-          recommendations.map((track) => (
-            <div className="card" key={track.id}>
-              <img src={track.album.images[0].url} alt={track.name} />
-              <p>{track.name}</p>
+    <div className="main-content">
+      <h1>Good afternoon, {user?.display_name}</h1>
+      <div className="recommendations-grid">
+        {recommendations.map((track) => (
+          <div className="track-card" key={track.id}>
+            <img src={track.album.images[0].url} alt={track.name} />
+            <div className="track-info">
+              <h3>{track.name}</h3>
               <p>{track.artists.map(artist => artist.name).join(', ')}</p>
             </div>
-          ))
-        ) : (
-          <p>No recommendations found</p> // Add a message if no recommendations are found
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
